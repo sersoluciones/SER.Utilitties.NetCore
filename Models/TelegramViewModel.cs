a64d1030083d497f0c762812d03ee39e5dc23bb4
Gustavo Adolfo Morales <tavomorales88@gmail.com>
2021-09-07 15:35:34 -0500

    telegram send msg

---
+++ Models/TelegramViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SER.Utilitties.NetCore.Models
+{
+    public class TelegramViewModel
+    {
+        [JsonPropertyName("chat_id")]
+        public string ChatId { get; set; }
+
+        [JsonPropertyName("parse_mode")]
+        public string ParseMode { get; set; } = "html";
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("reply_markup")]
+        public List<TelegramMarkup> ReplyMarkup { get; set; } 
+       
+    }
+
+    public class TelegramMarkup
+    {
+        [JsonPropertyName("inline_keyboard")]
+        public List<TelegramKeyboard> InlineKeyboard { get; set; }
+    }
+
+    public class TelegramKeyboard
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
--- Services/TelegramMessageWriter.cs
+++ Services/TelegramMessageWriter.cs
@@ -1,29 +1,78 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using SER.Utilitties.NetCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SER.Utilitties.NetCore.Services
 {
     public class TelegramMessageWriter
     {
-        private IConfiguration _config;
+        private readonly IConfiguration _config;
+        private readonly RestClient _client;
+        private const string BASE_URL = "https://api.telegram.org";
+        private readonly ILogger _logger;
 
-        public TelegramMessageWriter(IConfiguration config)
+        public TelegramMessageWriter(IConfiguration config, ILoggerFactory loggerFactory)
         {
             _config = config;
+            _client = new RestClient(BASE_URL);
+            _logger = loggerFactory.CreateLogger("TelegramMessageWriter");
         }
 
-        public async Task WriteMessage(string message, string channel)
+        public async Task WriteMessage(string message, string channel, string mode = "html")
         {
-            string Token = _config.GetSection("Telegram").GetSection("Token").Value;
-            string Channel = _config.GetSection("Telegram").GetSection("Channels").GetSection(channel).Value;
+            string token = _config.GetSection("Telegram").GetSection("Token").Value;
+            string channelId = _config.GetSection("Telegram").GetSection("Channels").GetSection(channel).Value;
 
-            HttpClient client = new HttpClient();
-            await client.GetAsync($"https://api.telegram.org/bot{ Token }/sendMessage?chat_id={ Channel }&parse_mode=Markdown&text={ message }");
+            await Execute(MakePostRequest(new TelegramViewModel
+            {
+                ChatId = channelId,
+                ParseMode = mode,
+                Text = message,
+            }, token));
+        }
+
+        private RestRequest MakePostRequest(TelegramViewModel model, string token)
+        {
+            var request = new RestRequest($"/bot{token}/sendMessage", Method.POST)
+            {
+                RequestFormat = DataFormat.Json,
+            };
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+            var jsonString = JsonSerializer.Serialize(model, options);
+            _logger.LogInformation($"Request:\n{jsonString}");
+
+            request.AddJsonBody(jsonString);
+            return request;
+        }
+
+        private async Task<bool> Execute(RestRequest request)
+        {
+            var response = await _client.ExecuteAsync(request);
+            try
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    return true;
+                return false;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                return false;
+            }
         }
     }
 }
